da6b041d96d0d7093a5b62bd75fd315b8843f46a
Yousuf <andromeda@andromeda-desktop.(none)>
2011-01-09 01:30:46 +0530

    Tab cross fade. :P

--- static/js/common/BoxTabbed.js
+++ static/js/common/BoxTabbed.js
@@ -8,12 +8,13 @@ $(document).ready(function() {
 	//On Click Event
 	$("ul.tabs li").click(function() {
 
+        var orig = $("ul.tabs li.active a").attr('href');
 		$("ul.tabs li").removeClass("active"); //Remove any "active" class
 		$(this).addClass("active"); //Add "active" class to selected tab
-		$(".tab_content").hide(); //Hide all tab content
 
 		var activeTab = $(this).find("a").attr("href"); //Find the href attribute value to identify the active tab + content
-		$(activeTab).fadeIn(1000); //Fade in the active ID content
+        $(orig).fadeOut(2000); //Hide all tab content
+		$(activeTab).fadeIn(2000); //Fade in the active ID content
 		return false;
 	});
     $(".tabs li a").tipTip({maxWidth: "auto"});
