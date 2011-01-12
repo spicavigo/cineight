1abd9afba8db27d59d8b96d13ee4392b25bcf4e9
Yousuf <andromeda@andromeda-desktop.(none)>
2011-01-12 17:32:22 +0530

    FB XFBML parse

--- static/js/common/TabDefault.js
+++ static/js/common/TabDefault.js
@@ -12,6 +12,7 @@ $(function(){
         $.get(AJAX_URL, {
             'data_id': data_id
         }, function(html) {
+            FB.XFBML.Host.parseDomElement(html);
             $(html).find("span.spoiler").hide();
             $(that).parents('.lzTab').find('.elements').append($(html).find('.elements').html());
             $(that).parents('.lzTab').find('.tabBtns').replaceWith($(html).find('.tabBtns'));
@@ -19,7 +20,7 @@ $(function(){
             $('textarea').autoResize();
             init_textarea();
             clicked = false;
-            FB.XFBML.parse();
+            //FB.XFBML.parse();            
         });
     });
 });
